e6e7e6b0fc500d86e130a11933b80ca6bf8e57a5
xv_rong <1187518864@qq.com>
2022-05-08 23:06:41 +0800

    fix fish

--- .config/fish/functions/config.fish
+++ .config/fish/functions/config.fish
@@ -1,3 +1,3 @@
-function config --wraps='/usr/bin/git --git-dir=$HOME/.myconf/ --work-tree=$HOME' --description 'alias config=/usr/bin/git --git-dir=$HOME/.myconf/ --work-tree=$HOME'
-  /usr/bin/git --git-dir=$HOME/.myconf/ --work-tree=$HOME $argv; 
+function config --wraps='/usr/bin/git --git-dir=$HOME/.myconfig_git/ --work-tree=$HOME' --description 'alias config=/usr/bin/git --git-dir=$HOME/.myconfig_git/ --work-tree=$HOME'
+  /usr/bin/git --git-dir=$HOME/.myconfig_git/ --work-tree=$HOME $argv; 
 end
