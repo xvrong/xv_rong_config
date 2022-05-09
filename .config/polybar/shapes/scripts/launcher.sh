9f87f8258326c9a1d2270ee0265ce7ca1f8e35c0
xv_rong <1187518864@qq.com>
2022-05-09 10:21:22 +0800

    stable

--- .config/polybar/shapes/scripts/launcher.sh
+++ .config/polybar/shapes/scripts/launcher.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
-rofi -no-config -no-lazy-grab -show drun -modi drun -theme ~/.config/polybar/shapes/scripts/rofi/launcher.rasi
+# rofi -no-config -no-lazy-grab -show drun -theme ~/.config/polybar/shapes/scripts/rofi/launcher.rasi
+rofi -no-config -no-lazy-grab -show combi -combi-modi "window,drun,ssh" -theme /home/xv_rong/.config/polybar/shapes/scripts/rofi/launcher.rasi
